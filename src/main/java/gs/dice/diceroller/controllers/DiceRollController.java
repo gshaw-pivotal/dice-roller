35f8c1e1f8895c25269b8e4d8839632c108aa224
gshaw-pivotal <gshaw+github@pivotal.io>
2018-03-01 17:54:27 -0500

    Now returns the dieType and correct number of rolls based on request
    A response to a roll request now returns the die type and a list of
    'results'. The list contains the number of results that matches with the
    number of rolls requested.
    
    The results an simply an incrementing number starting at one and thus
    can exceed the die type (eg. a 6 sided die will currently return roll
    results higher than 6 when more than 6 rolls are requested).

--- src/main/java/gs/dice/diceroller/controllers/DiceRollController.java
+++ src/main/java/gs/dice/diceroller/controllers/DiceRollController.java
@@ -15,6 +15,26 @@ public class DiceRollController {
 
     @RequestMapping(value = "/roll", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
     public String rollDiceRequest(@Valid @RequestBody DieRoll requestedDieRoll) {
-        return "1";
+        return buildResponse(requestedDieRoll);
+    }
+
+    private String buildResponse(DieRoll requestedDieRoll) {
+        return "{" +
+                "\"dieType\": " + requestedDieRoll.getDieType() + "," +
+                "\"results\": " + generateRolls(requestedDieRoll.getRollCount()) +
+                "}";
+    }
+
+    private String generateRolls(int numberOfRolls) {
+        String results = "[";
+
+        for (int count = 0; count < numberOfRolls - 1; count++) {
+            results = results + (count+1) + ",";
+        }
+
+        results = results + numberOfRolls;
+        results = results + "]";
+
+        return results;
     }
 }
--- src/test/java/gs/dice/diceroller/controllers/DiceRollControllerTest.java
+++ src/test/java/gs/dice/diceroller/controllers/DiceRollControllerTest.java
@@ -1,6 +1,7 @@
 package gs.dice.diceroller.controllers;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,10 +53,73 @@ public class DiceRollControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    public void post_receivesAValidRequestToRollASingleTypeOfDie_returnsResponseWithTheDieType() throws Exception {
+        int dieType = 6;
+        String expectedResponse = "\"dieType\": " + dieType;
+
+        MvcResult result = mockMvc.perform(
+                post("/roll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildValidSingleDieRollRequestBody(dieType)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(result.getResponse().getContentAsString(), containsString(expectedResponse));
+    }
+
+    @Test
+    public void post_receivesAValidRequestForMultipleRollsOfASingleDieType_returnsRollResultsAsAList() throws Exception {
+        String rollResultsPattern = ".*\"results\":\\s\\[[\\d,\\s]*].*";
+
+        MvcResult result = mockMvc.perform(
+                post("/roll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildValidMultiDieRollRequestBody(6, 2)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertTrue(result.getResponse().getContentAsString().matches(rollResultsPattern));
+    }
+
+    @Test
+    public void post_receivesAValidRequestForMultipleRollsOfASingleDieType_returnsCorrectNumberOfRollResults() throws Exception {
+        int numberOfRolls = 2;
+
+        String rollResultsSegmentPattern = "\"results\":\\s\\[[\\d,\\s]*]";
+        String rollResultsPattern = "\\[.*\\]";
+
+        Pattern resultSegmentPattern = Pattern.compile(rollResultsSegmentPattern);
+        Pattern resultRollPattern = Pattern.compile(rollResultsPattern);
+
+        MvcResult result = mockMvc.perform(
+                post("/roll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(buildValidMultiDieRollRequestBody(6, numberOfRolls)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Matcher resultSegmentMatcher = resultSegmentPattern.matcher(result.getResponse().getContentAsString());
+        resultSegmentMatcher.find();
+
+        Matcher rollResultsMatcher = resultRollPattern.matcher(resultSegmentMatcher.group(0));
+        rollResultsMatcher.find();
+        String rollResults = rollResultsMatcher.group(0).replaceAll("\\[", "").replaceAll("\\]", "");
+
+        assertThat(rollResults.split(",").length, equalTo(numberOfRolls));
+    }
+
     private String buildValidSingleDieRollRequestBody(int dieType) {
         return "{" +
                 "\"dieType\": " + dieType + "," +
                 "\"rollCount\": 1" +
+                "}";
+    }
+
+    private String buildValidMultiDieRollRequestBody(int dieType, int rollCount) {
+        return "{" +
+                "\"dieType\": " + dieType + "," +
+                "\"rollCount\": " + rollCount +
                 "}";
     }
 
