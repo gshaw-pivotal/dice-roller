0eeb11e5e6ea610d678b00a0308ce6bbba245c3f
gshaw-pivotal <gshaw+github@pivotal.io>
2018-03-19 00:38:25 -0400

    Refactor controller to include explicit cast from json to pojo
    Done to support upcoming work that will allow for the endpoint to handle
    either a single die roll request (the json form of a DieRoll) or a list
    of one or more die roll requests (the json form of a list if DieRoll
    objects).

--- src/main/java/gs/dice/diceroller/controllers/DiceRollController.java
+++ src/main/java/gs/dice/diceroller/controllers/DiceRollController.java
@@ -1,5 +1,7 @@
 package gs.dice.diceroller.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import gs.dice.diceroller.models.DieRoll;
 import gs.dice.diceroller.services.DiceRollService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -21,8 +24,20 @@ public class DiceRollController {
     private DiceRollService diceRollService;
 
     @RequestMapping(value = "/roll", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
-    public ResponseEntity<DieRoll> rollDiceRequest(@Valid @RequestBody DieRoll requestedDieRoll) {
-        DieRoll processedDieRoll = diceRollService.generateRolls(requestedDieRoll);
-        return new ResponseEntity(processedDieRoll, HttpStatus.OK);
+    public ResponseEntity<DieRoll> rollDiceRequest(@Valid @RequestBody JsonNode request) {
+        try {
+            DieRoll processedDieRoll = diceRollService.generateRolls(convertJsonToPojo(request));
+            return new ResponseEntity(processedDieRoll, HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity(
+                    "Invalid request body for a single die type roll request",
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private DieRoll convertJsonToPojo(JsonNode jsonNode) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        DieRoll dieRoll = mapper.readValue(jsonNode.toString(), DieRoll.class);
+        return dieRoll;
     }
 }
